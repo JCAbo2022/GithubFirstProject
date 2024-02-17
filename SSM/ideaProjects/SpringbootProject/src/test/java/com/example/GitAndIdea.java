3972beda55aa591cd6b6c041b2158a9340bd6da3
Abo <3222786558@qq.com>
2024-02-17 12:05:39 +0000

    gitee在线修改提交
    Signed-off-by: Abo <3222786558@qq.com>

--- SSM/ideaProjects/SpringbootProject/src/test/java/com/example/GitAndIdea.java
+++ SSM/ideaProjects/SpringbootProject/src/test/java/com/example/GitAndIdea.java
@@ -13,5 +13,8 @@ public class GitAndIdea {
         System.out.println("push test!!!!!!!!");
         System.out.println("pull test!!!!!!!!");
         System.out.println("gitee test!!!!!!!!");
+        System.out.println("push test!!!!!!!!");
+        System.out.println("pull test!!!!!!!!");
+        System.out.println("gitee test!!!!!!!!");
     }
 }
