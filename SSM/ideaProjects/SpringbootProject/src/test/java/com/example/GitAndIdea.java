7f2d7ba1383e35ecaec82c2980b03e25866cf387
jiangcaiabo <3222786558@qq.com>
2024-02-17 17:13:34 +0800

    第二次提交！

--- SSM/ideaProjects/SpringbootProject/src/test/java/com/example/GitAndIdea.java
+++ SSM/ideaProjects/SpringbootProject/src/test/java/com/example/GitAndIdea.java
@@ -5,5 +5,6 @@ import org.apache.ibatis.logging.stdout.StdOutImpl;
 public class GitAndIdea {
     public static void main(String[] args) {
         System.out.println("打印IDEA结合git一起使用的演示！！！！！！！！！");
+        System.out.println("打印IDEA结合git一起使用的演示！！！！！！！！！");
     }
 }
